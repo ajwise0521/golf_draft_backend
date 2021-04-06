858c7039c62fe83713cd1116d7c9ad79e97c845c
Andrew Wise <ajwise0521@gmail.com>
2021-04-06 14:26:04 -0500

    randomizing

--- app/Helpers/createSnakeDraftOrder.php
+++ app/Helpers/createSnakeDraftOrder.php
@@ -8,7 +8,7 @@ use App\Models\LeagueMember;
 class createSnakeDraftOrder {
 	
 	function getLeaguePlayers($leagueId) {
-		$members = LeagueMember::where('league_id', $leagueId)->get()->toArray();
+		$members = LeagueMember::where('league_id', $leagueId)->inRandomOrder()->get()->toArray();
 
 		return $members;
 	}
